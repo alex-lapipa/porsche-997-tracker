198e0230a50a407b1d5f28ef764f644a5cf38ed7
Alex Lawton <lawton.alex@gmail.com>
2025-08-02 03:15:27 +0000

    Add Vercel deployment config

--- .next/static/KFCfOItg0SvzjfjaM7kz-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-4f348b2ad8531e43.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
